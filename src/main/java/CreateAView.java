053db0c30da25168ba6815317ca0c126b0ec0f9e
onusra <onusra@angelo.edu>
2023-07-12 16:40:43 +0000

    completed

--- src/main/java/CreateAView.java
+++ src/main/java/CreateAView.java
@@ -67,6 +67,8 @@ public class CreateAView {
         
     }
 
+    
+
     public void problem1(){
         /**
          * problem1: Create a view called "firstname_lastname" in problem1.sql from the site_user table that only has the firstname and lastname columns.
